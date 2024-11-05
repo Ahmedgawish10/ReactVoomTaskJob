05f68e4c12b8a6cc93b259365ea87954e21961e7
ahmedgawish10 <ahmdgawish44@gmail.com>
2024-11-05 02:07:55 +0200

    part one of task in progress for performance

---
+++ src/type/types.tsx
@@ -0,0 +1,9 @@
+interface InterfaceDataPoly {
+    code: number;
+    status: string;
+    price: number;
+    area?: string;
+    unitType?: string;
+    unit?:string
+  };
+  export default InterfaceDataPoly;
